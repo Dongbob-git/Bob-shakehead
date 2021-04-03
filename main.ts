129da5207e9ad85a7068309401c420c7bc546427
Dongbob-git <76213782+Dongbob-git@users.noreply.github.com>
2021-04-03 12:35:58 +0800

    MakeCode 项目的初始文件

---
+++ main.ts
@@ -0,0 +1,39 @@
+input.onGesture(Gesture.TiltLeft, function () {
+    basic.showLeds(`
+        . . # . .
+        . # . . .
+        # # # # #
+        . # . . .
+        . . # . .
+        `)
+})
+input.onGesture(Gesture.TiltRight, function () {
+    basic.showLeds(`
+        . . # . .
+        . . . # .
+        # # # # #
+        . . . # .
+        . . # . .
+        `)
+})
+input.onGesture(Gesture.ScreenUp, function () {
+    basic.showLeds(`
+        . . # . .
+        . . # . .
+        # # # # #
+        . . # . .
+        . . # . .
+        `)
+})
+basic.clearScreen()
+basic.showLeds(`
+    . # # . .
+    . # . # .
+    . # . # .
+    . # . # .
+    . # # . .
+    `)
+basic.pause(100)
+basic.forever(function () {
+    soundExpression.giggle.playUntilDone()
+})
